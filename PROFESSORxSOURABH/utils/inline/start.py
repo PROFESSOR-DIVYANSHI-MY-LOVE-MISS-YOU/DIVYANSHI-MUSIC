98f8f352436e118dc5183e251f26027f99338ca9
#1 on the leaderboard <professor4testing@gmail.com>
2024-12-31 11:28:18 +0530

    [ 𝗣𝗥𝚯𝗙𝗘𝗦𝗦𝚯𝗥 ][ ѕαиαтαиι ]

--- PROFESSORxSOURABH/utils/inline/start.py
+++ PROFESSORxSOURABH/utils/inline/start.py
@@ -26,12 +26,12 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_11"], web_app=WebAppInfo(url=config.PROFESSORRR)),
-            InlineKeyboardButton(text=_["S_B_12"], web_app=WebAppInfo(url=config.PROFESSORSTATUS)),
+            InlineKeyboardButton(text=_["S_B_11"], web_app=WebAppInfo(url=config.RRR)),
+            InlineKeyboardButton(text=_["S_B_12"], web_app=WebAppInfo(url=config.STATUS)),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_13"], web_app=WebAppInfo(url=config.PROFESSORBOTS)),
-            InlineKeyboardButton(text=_["S_B_14"], web_app=WebAppInfo(url=config.PROFESSORSMM)),
+            InlineKeyboardButton(text=_["S_B_13"], web_app=WebAppInfo(url=config.BOTS)),
+            InlineKeyboardButton(text=_["S_B_14"], web_app=WebAppInfo(url=config.SMM)),
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], web_app=WebAppInfo(url=config.PROFESSOR)),
